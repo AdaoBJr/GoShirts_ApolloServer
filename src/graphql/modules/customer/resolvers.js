47ff419153f796bfc0757477bdc5d31ca478a03b
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-20 16:52:26 -0400

    feat: :sparkles: create query and mutation customer

---
+++ src/graphql/modules/customer/resolvers.js
@@ -0,0 +1,14 @@
+const customerdb = [];
+
+export default {
+  Query: {
+    customerList: async () => customerdb,
+  },
+  Mutation: {
+    createCustomer: async (_parent, args, _context, _info) => {
+      const { data } = args;
+      customerdb.push(data);
+      return data;
+    },
+  },
+};
