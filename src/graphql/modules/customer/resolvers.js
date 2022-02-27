0934bfebde0d38396f49366e41c3ef2b4ebcda30
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-27 18:04:50 -0400

    feat: :sparkles: add customerById on Resolvers

--- src/graphql/modules/customer/resolvers.js
+++ src/graphql/modules/customer/resolvers.js
@@ -1,9 +1,10 @@
 import { useCustomer } from '../../../middlewares/talons/useCustomer';
-const { CreateCustomer, customerList } = useCustomer();
+const { CustomerList, CustomerById, CreateCustomer } = useCustomer();
 
 export default {
   Query: {
-    customerList: async () => await customerList(),
+    customerList: async () => await CustomerList(),
+    customerById: async (_, { data }) => await CustomerById({ data }),
   },
   Mutation: {
     createCustomer: async (_, { data }) => await CreateCustomer({ data }),
