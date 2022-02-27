2b28a1ae80194ddf6f137d9827611157f79ee793
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-27 18:05:39 -0400

    feat: :sparkles: add CustomerByID on useCustomer

--- src/middlewares/talons/useCustomer.js
+++ src/middlewares/talons/useCustomer.js
@@ -1,16 +1,21 @@
 import Customer from '../../repositories/mongodb/models/customer';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
 
 const useCustomer = () => {
-  const customerList = async () => await Customer.find();
+  const CustomerList = async () => await Customer.find();
+
+  const CustomerById = async ({ data }) => ({
+    customer: await Customer.findOne({ id: data.id }).exec(),
+  });
 
   const CreateCustomer = async ({ data }) => {
-    const id = uuidv4();
+    const id = uuidV4();
     const customerData = { id, ...data };
     const customer = await Customer.create(customerData);
     return { customer };
   };
-  return { CreateCustomer, customerList };
+
+  return { CustomerList, CustomerById, CreateCustomer };
 };
 
 export { useCustomer };
