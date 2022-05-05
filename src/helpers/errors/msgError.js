7220a2e005f45a4e52ed3153d2b4535c7301a58c
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-04 21:16:21 -0400

    feat: :zap: add new msg error

--- src/helpers/errors/msgError.js
+++ src/helpers/errors/msgError.js
@@ -1,3 +1,4 @@
 export const emailExists = { code: 409, msg: 'E-mail already registered' };
 export const userDoesNotExist = { code: 401, msg: 'Data entry incorrect' };
 export const emailOrPwdIncorrect = { code: 401, msg: 'E-mail or Password incorrect' };
+export const expiredSession = { code: 401, msg: 'Expired Session' };
