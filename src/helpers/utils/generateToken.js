3f6a30b614448b65bb8138c3635e351e3b1a241c
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-04 21:14:33 -0400

    refactor: :recycle: refact with second option generateToken

--- src/helpers/utils/generateToken.js
+++ src/helpers/utils/generateToken.js
@@ -1,5 +1,6 @@
 import { sign } from 'jsonwebtoken';
 import CustomerTokensRepository from '../../repositories/mongodb/models/customerTokens';
+import decodeToken from './decodeToken';
 
 const { JWT_SECRET } = process.env;
 
@@ -8,11 +9,44 @@ const jwtConfig = {
   expiresIn: '10m',
 };
 
-const generateToken = async ({ id }) => {
-  const token = sign({ id }, JWT_SECRET, jwtConfig);
-  const items = [token];
-  const customerToken = { userId: id, items, count: items.length };
-  await CustomerTokensRepository.create(customerToken);
+// First option for generate token
+
+// const generateToken = async ({ id }) => {
+//   const token = sign({ id }, JWT_SECRET, jwtConfig);
+//   const items = [token];
+//   const customerToken = { userId: id, items, count: items.length };
+//   await CustomerTokensRepository.create(customerToken);
+//   return token;
+// };
+
+const generateToken = async ({ userId }) => {
+  const CustomerToken = await CustomerTokensRepository.findOne({ userId }).exec();
+
+  const checkTokens =
+    CustomerToken &&
+    CustomerToken.items
+      .map((token) => {
+        const decodedToken = decodeToken({ token, genNewToken: true });
+        return decodedToken.expired ? false : token;
+      })
+      .filter((item) => item);
+
+  const token = sign({ id: userId }, JWT_SECRET, jwtConfig);
+
+  const items = [];
+  if (!checkTokens) items.push(token);
+  if (checkTokens) items = [...checkTokens, token];
+
+  const customerToken = { userId, items, count: items.length };
+
+  if (checkTokens) {
+    await CustomerTokensRepository.findByIdAndUpdate({ userId }, customerToken, {
+      new: true,
+    });
+  } else {
+    await CustomerTokensRepository.create(customerToken);
+  }
+
   return token;
 };
 
